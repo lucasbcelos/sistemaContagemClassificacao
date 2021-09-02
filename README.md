9a8372def88cd843b0d48d43b0187049e75070fb
lucasbcelos <lucasbs302@gmail.com>
2021-09-01 21:16:50 -0300

    Commit inicial, subindo arquivos README.md e sistema.js

---
+++ README.md
@@ -0,0 +1,11 @@
+## ATIVIDADE ENCONTRO REMOTO 2 - UC8 - LOGICA DE PROGRAMAÇÃO
+
+###  __Criação de um sistema de Contagem e Classificação de Alunos__ 
+
+<p> O sistema deve funcionar da seguinte forma:
+
+* Verificar a quantidade de alunos na sala
+* Percorrer do zero até o numero de alunos
+* Se o numero for par, escrever 'par' e o numero correspondente
+* Se o numero for impar, escrever 'impar' e o numero correspondente
+* Se o numero for zero, escrever 'zero' 
---
+++ sistema.js
@@ -0,0 +1,26 @@
+//Inicio da Atividade - Definir o numero de Alunos
+
+let numeroDeAlunos = 10
+
+//Percorrendo os numeros de aluno, do zero até a quantidade de alunos
+
+for (let contador = 0; contador <= numeroDeAlunos; contador++) {
+    
+    // Condição para quando for zero
+
+    if (contador === 0) {
+        console.log("O numero atual é zero")
+
+      // Condição para quando for par, o resto da divisão por 2 é igual a zero
+
+    } else if (contador % 2 == 0) {
+        console.log("O numero " + contador + " é PAR")
+        
+        // Condição para quando for impar, o resto da divisão por dois é diferente de zero,
+        // ou então é toda situação que não for nem zero e nem par  
+        
+      } else {
+          console.log("O numero " + contador + " é IMPAR")
+      }
+  }
+        
